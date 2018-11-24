f90e4e266c7d66133552196128dabebe2952d10d
esosebee5 <esosebee114@gmail.com>
2018-11-24 10:19:56 -0500

    final? in sync

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
+// full functionality only works properly in the Microsoft Edge browser
+
 YUI().use("node", function(Y) {
 
     var name;
-    var state = 1;
 
     var COMMANDS = [
-        {
-            name: "do_stuff",
-            handler: doStuff
-        },
         {
             name: "1",
             handler: clock
@@ -16,30 +13,14 @@ YUI().use("node", function(Y) {
             name: "2",
             handler: calculateAge
         },
-
         {
-            name: "greet",
-            handler: function(args) {
-                outputToConsole("Hello " + args[0] + ", welcome to Console.");
+            name: "end",
+            handler: function(e) {
+                outputToConsole("User ended process.");
+                window.close();
             }
         }
     ];
-
-    function startBot() {
-
-        if (name == null) {
-            name = getInputLine();
-            outputToConsole("Hello, " + name + ".<br><br>");
-        } else {
-            processCommand();
-        }
-
-        // while (state == 1) {
-            outputToConsole("Enter '1' for Clock, '2' to Calculate Age, or");
-            outputToConsole("enter 'end' to terminate: ");
-        //}
-
-    }
 
     function getInputLine() {
         // get
@@ -54,31 +35,132 @@ YUI().use("node", function(Y) {
         return parts;
     }
 
-    function processCommand() {
-        var parts = getInputLine();
-        var command = parts[0];
-        var args = parts.length > 1 ? parts.slice(1, parts.length) : [];
+    function processInput() {
 
-        for (var i = 0; i < COMMANDS.length; i++) {
-            if (command === COMMANDS[i].name) {
-                COMMANDS[i].handler(args);
-                return;
+        var parts = getInputLine();
+
+        if (parts.length == 1) {
+            var arg = parts[0];
+
+            for (var i = 0; i < COMMANDS.length; i++) {
+                if (arg === COMMANDS[i].name) {
+                    COMMANDS[i].handler(arg);
+                    return;
+                }
             }
         }
-
-        outputToConsole("Unsupported Command: " + command);
-    }
-
-    function doStuff(args) {
-        outputToConsole("I'll just echo the args: " + args);
     }
 
     function clock() {
-        outputToConsole("The current time is: ");
+        startApp();
+
+        var dateTime = new Date();
+        var time = dateTime.getHours() + ":" + dateTime.getMinutes() 
+            + ":" + dateTime.getSeconds();
+        outputToConsole("The current time is: " + time.toString());
+        
+        endApp();
     }
 
     function calculateAge() {
-        outputToConsole("You are _ years old.");
+        startApp();
+
+        // current date
+        var currentDate = new Date();
+        var currentMonth = currentDate.getMonth() + 1;
+        var currentDay = currentDate.getDate();
+        var currentYear = currentDate.getFullYear();
+
+        var birthMonth = getBirthMonth();
+        //outputToConsole("birth month number: " + birthMonth);
+        
+        var birthDay = getBirthDay(birthMonth);
+        //outputToConsole("birth day: " + birthDay);
+
+        var birthYear = getBirthYear(currentYear);
+        //outputToConsole("birth year: " + birthYear);
+
+        var age = 0;
+        if (birthMonth < currentMonth || (birthMonth == currentMonth && birthDay <= currentDay)) {
+            age = currentYear - birthYear;
+        } else {
+            age = currentYear - birthYear - 1;
+        }
+
+        outputToConsole("You are " + age + " years old.");
+        
+        endApp();
+    }
+
+    function getBirthMonth() {
+        var month = 0;
+
+        while (month == 0) {
+            var monthStr = prompt("Enter your birth month: ");
+            monthStr = monthStr.toLowerCase();
+            month = monthStringToNumber(monthStr);
+        }
+
+        return month;
+    }
+
+    function getBirthDay(month) {
+        var day = 0;
+        var maxDays = 31;
+
+        if (month == 2) {
+            maxDays = 28;
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            maxDays = 30;
+        }
+
+        while (day == 0) {
+            day = prompt("Enter the day of the month: ");
+
+            if (day < 1 || day > maxDays) {
+                day = 0;
+            }
+        }
+
+        return day;
+    }
+
+    function getBirthYear(currentYear) {
+        var year = 0;
+
+        while (year == 0) {
+            year = prompt("Enter your birth year: ");
+
+            if (year < 1 || year > currentYear) {
+                year = 0;
+            }
+        }
+
+        return year;
+    }
+
+    function monthStringToNumber(input) {
+        var string = input.toLowerCase();
+        var shortMonths = ["jan", "feb", "mar", "apr", "may", "jun", 
+                        "jul", "aug", "sep", "oct", "nov", "dec"];
+        var fullMonths = ["january", "february", "march", "april", "may", 
+                        "june", "july", "august", "september", "october", 
+                        "november", "december"];
+        
+        for (var i = 0; i < 12; i++) {
+            if (string === shortMonths[i].toString() || string === fullMonths[i].toString()) {
+                return i + 1;
+            }
+        }
+        return 0;
+    }
+
+    function startApp() {
+        outputToConsole("--------------------------------<br>")
+    }
+
+    function endApp() {
+        outputToConsole("--------------------------------<br><br>")
     }
 
     function outputToConsole(text) {
@@ -89,10 +171,25 @@ YUI().use("node", function(Y) {
 
     Y.on("domready", function(e) {
         Y.one("body").setStyle("paddingBottom", Y.one("#in").get("offsetHeight"));
+
+        // greeting
+        var p = Y.Node.create("<p>Welcome to the JavaScipt HelloBot Project!</p><br>");
+        Y.one("#out").append(p);
+        
+        // get name
+        name = prompt("Enter name:");
+        outputToConsole("Hello, " + name + ".<br><br>");
+
+        // loop through programs
+        outputToConsole("Enter '1' for Clock, '2' to Calculate Age, or");
+        outputToConsole("enter 'end' to terminate: <br><br>");
         Y.one("#in").on("keydown", function(e) {
             if (e.charCode === 13) {
-                startBot();
-                // processCommand();
+                processInput();
+
+                // set up next loop
+                outputToConsole("Enter '1' for Clock, '2' to Calculate Age, or");
+                outputToConsole("enter 'end' to terminate: <br><br>");
             }
         });
     });
--- src/start.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    
-    $("#out").append(`<p>Welcome to the JavaScipt HelloBot Project!</p>
-                    <br><br>
-                    <p>Enter your name:</p>`);
-})
